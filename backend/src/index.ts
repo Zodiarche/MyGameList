1eda57b8ab2658196f2e9c89277c8366143a43e3
Zozo <benbar2412@gmail.com>
2025-09-17 20:55:35 +0200

    chore(backend): Initialisation NodeJS Express Typescript

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import healthRouter from "./routes/health";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/health", healthRouter);
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on http://localhost:${port}`);
+});
